c105da5732f787d7348b088be0dd97aee805c0dc
Arthur Nurgaliev <iamflyyy@gmail.com>
2023-11-07 20:28:07 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module wb-tech-level-0
+
+go 1.21
---
+++ internal/storage/migrations/20231107185153_init.sql
@@ -0,0 +1,63 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE orders (
+    order_uid VARCHAR PRIMARY KEY,
+    track_number VARCHAR,
+    entry VARCHAR,
+    locale VARCHAR,
+    customer_id VARCHAR,
+    date_created TIMESTAMP,
+    oof_shard VARCHAR
+);
+
+CREATE TABLE delivery (
+    order_uid VARCHAR PRIMARY KEY,
+    name VARCHAR,
+    phone VARCHAR,
+    zip VARCHAR,
+    city VARCHAR,
+    address VARCHAR,
+    region VARCHAR,
+    email VARCHAR,
+    FOREIGN KEY (order_uid) REFERENCES orders(order_uid)
+);
+
+CREATE TABLE payment (
+    order_uid VARCHAR PRIMARY KEY,
+    transaction VARCHAR,
+    request_id VARCHAR,
+    currency VARCHAR,
+    provider VARCHAR,
+    amount INT,
+    payment_dt TIMESTAMP,
+    bank VARCHAR,
+    delivery_cost INT,
+    goods_total INT,
+    custom_fee INT,
+    FOREIGN KEY (order_uid) REFERENCES orders(order_uid)
+);
+
+CREATE TABLE order_items (
+    order_uid VARCHAR PRIMARY KEY,
+    chrt_id INT,
+    track_number VARCHAR,
+    price INT,
+    rid VARCHAR,
+    name VARCHAR,
+    sale INT,
+    size VARCHAR,
+    total_price INT,
+    nm_id INT,
+    brand VARCHAR,
+    status INT,
+    FOREIGN KEY (order_uid) REFERENCES orders(order_uid)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS delivery;
+DROP TABLE IF EXISTS payment;
+DROP TABLE IF EXISTS order_items;
+-- +goose StatementEnd
